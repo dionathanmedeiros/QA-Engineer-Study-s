9fcf9b6e5e82a25cf933b5d63829dc43d8e80bfe
dionathanmedeiros <dionimedeiros@gmail.com>
2024-11-01 18:07:09 -0300

    Added My info complete test / Custom Fields Complete test

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -14,20 +14,28 @@ describe('Orange HRM Tests', () => {
       middleName: "[name='middleName']",
       lastName: "[name='lastName']",
       genericField:".oxd-input--active",
-      dateField:"[placeholder='yyyy-dd-mm']",
+      dateField:"[placeholder='D, dd M yyyy']",
       dateCloseButton:(".--close"),
-      buttonSubmit:("[type='submit']")
+      buttonSubmit:("[type='submit']"),
+      buttonDown:(".oxd-select-text--arrow"),
+      nationality:(':nth-child(27)'),
+      maritalStatus:('.oxd-select-dropdown > :nth-child(3)'),
+      gender:(".oxd-radio-wrapper"),
+      bloodType:('.oxd-select-dropdown > :nth-child(6)'),
+      testField:("[options='']")
 
 
   }
 
   it.only('Login - Sucess', () => {
+    //               Login Success
     cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
     cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get(selectorsList.dashboardGrid)
+    //                My Info
     cy.get(selectorsList.myInfoButton).click()
     cy.get(selectorsList.firstName).clear().type("creito")
     cy.get(selectorsList.middleName).clear().type("aparecido")
@@ -35,15 +43,32 @@ describe('Orange HRM Tests', () => {
     cy.get(selectorsList.genericField).eq(3).clear().type ("40028922")
     cy.get(selectorsList.genericField).eq(4).clear().type ("Other Id Test")
     cy.get(selectorsList.genericField).eq(5).clear().type ("Driver License Test")
-    cy.get(selectorsList.dateField).eq(0).clear().type("2024-10-30")
+    cy.get(selectorsList.dateField).eq(0).clear().type("2025-10-30")
     cy.get(selectorsList.dateCloseButton).click()
+    cy.get(selectorsList.buttonDown).eq(0).click()
+    cy.get(selectorsList.nationality).click()
+    cy.get(selectorsList.buttonDown).eq(1).click()
+    cy.get(selectorsList.maritalStatus).click()
+    cy.get(selectorsList.dateField).eq(1).clear().type("1999-12-12")
+    cy.get(selectorsList.dateCloseButton).click()
+    cy.get(selectorsList.gender).eq(0).click()
     cy.get(selectorsList.buttonSubmit).eq(0).click()
     cy.get('body').should('contain', 'Successfully Update')
     cy.get('.oxd-toast-close')
+//                Custom Fields
+    cy.get(selectorsList.buttonDown).eq(2).click()
+    cy.get(selectorsList.bloodType).click()
+    cy.get(selectorsList.testField).clear().type("777")
+    cy.get(selectorsList.buttonSubmit).eq(1).click()
+    cy.get('body').should('contain', 'Successfully Update')
+    cy.get('.oxd-toast-close')
+
+    
     
 
 
   })
+  //              Login Fail
   it('Login - Fail', () => {
     cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userFail.username)
